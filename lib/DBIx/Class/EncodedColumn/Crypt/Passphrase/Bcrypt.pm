4177e3f52e0f5142ce37646ae14e231e9145d9ed
Gene Boggs <gene@ology.net>
2022-12-22 18:55:22 -0800

    Bump version

--- lib/DBIx/Class/EncodedColumn/Crypt/Passphrase/Bcrypt.pm
+++ lib/DBIx/Class/EncodedColumn/Crypt/Passphrase/Bcrypt.pm
@@ -5,7 +5,7 @@ package DBIx::Class::EncodedColumn::Crypt::Passphrase::Bcrypt;
 use strict;
 use warnings;
 
-our $VERSION = '0.0104';
+our $VERSION = '0.0105';
 
 use Encode qw(encode_utf8);
 use Crypt::Passphrase::Bcrypt ();
